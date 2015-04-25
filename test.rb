8dc25601291bb31a4f0cd47722f4e0c5ed448981
carlmcateer <carlmc12@gmail.com>
2015-04-25 14:26:38 +0100

    first branch

--- test.rb
+++ test.rb
@@ -8,5 +8,14 @@ puts name.upcase.reverse
 
 puts name.downcase
 
-puts "What age are you?"
-user_age = gets.chomp
+print "What's your first name?"
+first_name = gets.chomp.capitalize!
+print "And your last name?"
+last_name = gets.chomp.capitalize!
+print "From what city do you hail?"
+city = gets.chomp.capitalize!
+print "Sorry what state is that in?"
+state = gets.chomp.capitalize!
+
+
+puts "I #{first_name} #{last_name} hearby state that #{city} (the finest town in all of #{state}) it the king of all tawns"
