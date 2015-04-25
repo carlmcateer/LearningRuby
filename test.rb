80d65d8aeacbf75b59cf9bb0eb3b46b2b8a799cb
carlmcateer <carlmc12@gmail.com>
2015-04-25 14:31:25 +0100

    test

--- test.rb
+++ test.rb
@@ -9,13 +9,16 @@ puts name.upcase.reverse
 puts name.downcase
 
 print "What's your first name?"
-first_name = gets.chomp.capitalize!
+first_name = gets.chomp
+first_name.capitalize!
 print "And your last name?"
-last_name = gets.chomp.capitalize!
+last_name = gets.chomp
+last_name.capitalize!
 print "From what city do you hail?"
-city = gets.chomp.capitalize!
+city = gets.chomp
+city.capitalize!
 print "Sorry what state is that in?"
-state = gets.chomp.capitalize!
-
+state = gets.chomp
+state.upcase!
 
 puts "I #{first_name} #{last_name} hearby state that #{city} (the finest town in all of #{state}) it the king of all tawns"
